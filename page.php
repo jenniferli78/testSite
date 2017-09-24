c41281ced037735c0bc6eb47c36a32514bc8f386
jenniferli <jenniferli.email@yahoo.com>
2017-09-23 23:30:47 -0700

    make it better

---
+++ page.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "I'm doin a test<br>";
+        ?>
+        <a href="index.php">HOME</a>
+    </body>
+</html>
